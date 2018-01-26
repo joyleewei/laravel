b49f8562ee481c61a6bd2ce40f0a274a06035436
joyleewei <1515885616@qq.com>
2018-01-26 14:02:03 +0800

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,12 +1,54 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller{
-    //创建用户
+    // 显示用户列表
+    public function index(){
+        return view('users.index');
+    }
+
+    // 显示用户信息
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+
+    //创建用户页面
     public function create(){
         return view('users.create');
     }
+
+    // 创建用户提交---POST
+    public function store(Request $request){
+        $this->validate($request,array(
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ));
+        $user = User::create(array(
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ));
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]);
+    }
+
+    // 编辑用户信息页面
+    public function edit(){
+
+    }
+
+    // 更新用户
+    public function update(){
+
+    }
+
+    // 删除用户
+    public function delete(){
+
+    }
+
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','用户列表页面')
+@section('content')
+<h1>axxxxxxxxx</h1>
+@stop
